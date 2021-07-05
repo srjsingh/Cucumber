ff88061f6cdd3c76e097a657106ec584c2d6b566
srjsingh <Srjsingh636@gmail.com>
2021-07-05 23:22:57 +0530

    changes

--- YatraBDDFramework/src/main/java/Runner/TestRunner.java
+++ YatraBDDFramework/src/main/java/Runner/TestRunner.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "C:\\Users\\Suraj\\git\\Cucumber\\YatraBDDFramework\\src\\main\\java\\Features\\FlightBooking.feature"
-		,glue = {"C:\\Users\\Suraj\\git\\Cucumber\\YatraBDDFramework\\src\\main\\java\\stepDefinitions\\FlightBookingStepDefinition.java"}
+		,glue = {"stepDefinitions"}
 		,format = {"pretty", "html:test-output", "json:json_output/cucumber.json", "junit:junit_xml/cucumber.xml"}
 		,strict = true
 		,monochrome = true
