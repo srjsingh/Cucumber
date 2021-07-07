bda32248e111461985cb303d3d3dd6289e4e1bf9
srjsingh <Srjsingh636@gmail.com>
2021-07-07 16:58:43 +0530

    comment

--- YatraBDDFramework/src/main/java/Runner/TestRunner.java
+++ YatraBDDFramework/src/main/java/Runner/TestRunner.java
@@ -8,11 +8,12 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "C:\\Users\\Suraj\\git\\Cucumber\\YatraBDDFramework\\src\\main\\java\\Features\\Tagging.feature"
-		,glue = "C:\\Users\\Suraj\\git\\Cucumber\\YatraBDDFramework\\src\\main\\java\\stepDefinitions\\TaggingStepDefinition.java"
+		,glue = {"stepDefinitions"}
 		,format = {"pretty", "html:test-output", "json:json_output/cucumber.json", "junit:junit_xml/cucumber.xml"}
 		,strict = true
 		,monochrome = true
 		,dryRun = true
+		//,tags = {"@SmokeTest"} // to run specific scenarios with matching tags to skip use ~ before @
 		)
 
 public class TestRunner {
