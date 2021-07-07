d009a1d7dfff9b9e80fc79a32e3a332a065fde2b
srjsingh <Srjsingh636@gmail.com>
2021-07-07 16:22:45 +0530

    tagging concept

---
+++ YatraBDDFramework/src/main/java/Features/Tagging.feature
@@ -0,0 +1,74 @@
+@FunctionalTest
+Feature: Yatra Test
+
+@SmokeTest @RegressionTest
+Scenario: Login with valid credentials1
+Given This is valid login test1
+
+@RegressionTest
+Scenario: Login with invalid credentials2
+Given This is invalid login test2
+
+@RegressionTest
+Scenario: Book a flight3
+Given Booking a flight3
+
+@SmokeTest
+Scenario: Login with valid credentials4
+Given This is valid login test4
+
+@SmokeTest
+Scenario: Login with invalid credentials5
+Given This is invalid login test5
+
+@RegressionTest
+Scenario: Book a flight6
+Given Booking a flight6
+
+@RegressionTest
+Scenario: Login with valid credentials7
+Given This is valid login test7
+
+@RegressionTest
+Scenario: Login with invalid credentials8
+Given This is invalid login test8
+
+@SmokeTest @RegressionTest
+Scenario: Book a flight9
+Given Booking a flight9
+
+@SmokeTest @RegressionTest
+Scenario: Login with valid credentials10
+Given This is valid login test10
+
+@SmokeTest @RegressionTest
+Scenario: Login with invalid credentials11
+Given This is invalid login test11
+
+@SmokeTest @EndToEndTest
+Scenario: Book a flight12
+Given Booking a flight12
+
+@SmokeTest @EndToEndTest
+Scenario: Login with valid credentials13
+Given This is valid login test13
+
+@SmokeTest @EndToEndTest
+Scenario: Login with invalid credentials14
+Given This is invalid login test14
+
+@SmokeTest @EndToEndTest
+Scenario: Book a flight15
+Given Booking a flight15
+
+@EndToEndTest
+Scenario: Login with valid credentials16
+Given This is valid login test16
+
+@EndToEndTest
+Scenario: Login with invalid credentials17
+Given This is invalid login test17
+
+@EndToEndTest
+Scenario: Book a flight18
+Given Booking a flight18
--- YatraBDDFramework/src/main/java/stepDefinitions/FlightBookingMapStepDefinition.java
+++ YatraBDDFramework/src/main/java/stepDefinitions/FlightBookingMapStepDefinition.java
@@ -1,97 +1,96 @@
-package stepDefinitions;
-
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-
-import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import cucumber.api.DataTable;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
-
-public class FlightBookingMapStepDefinition {
-	
-	//data table with maps for parameterization
-
-	WebDriver driver;
-
-	@Given("^User is already on Welcome Page$")
-	public void user_already_on_welcome_page() {
-
-		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\Suraj\\Desktop\\Selenium Jar\\Chrome Driver\\chromedriver.exe");
-
-		driver = new ChromeDriver();
-
-		driver.manage().window().maximize();
-		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-
-		driver.get("http://www.yatra.com");
-
-	}
-
-	@When("^title of login page is Yatra$")
-	public void title_of_welcome_page_is_yatra() {
-		String title = driver.getTitle();
-		System.out.println(title);
-		Assert.assertEquals("Flight, Cheap Air Tickets , Hotels, Holiday, Trains Package Booking - Yatra.com", title);
-	}
-
-	@Then("^User clicks on login My Account dropdown$")
-	public void user_clicks_on_login_My_Account_dropdown() {
-		driver.findElement(By.xpath("//a[@class='dropdown-toggle' and text()='My Account']")).click();
-	}
-
-	@Then("^login button is visible$")
-	public void login_button_is_visible() {
-		boolean loginButton = driver.findElement(By.id("signInBtn")).isDisplayed();
-		Assert.assertEquals(true, loginButton);
-	}
-
-	@Then("^User clicks login button$")
-	public void loginButtonClickTest() {
-		driver.findElement(By.id("signInBtn")).click();
-	}
-
-	@Then("user enters username$")
-	public void userNameFieldTest(DataTable username) {
-
-		for(Map<String, String> data1 : username.asMaps(String.class, String.class))
-		driver.findElement(By.xpath("//input[@id='login-input']")).sendKeys(data1.get("username"));
-	}
-
-	@Then("^user clicks on cont button$")
-	public void continueButtonTest() {
-		driver.findElement(By.xpath("//button[@id='login-continue-btn']")).click();
-
-	}
-
-	@Then("user enters password$")
-	public void passwordFieldTest(DataTable password) {
-		for (Map<String, String> data2 : password.asMaps(String.class, String.class)) {
-			
-			driver.findElement(By.xpath("//input[@id='login-password']")).sendKeys(data2.get("password"));
-		
-		}
-	}
-
-	@Then("^user clicks on login button on login page$")
-	public void loginButtonTest() {
-		driver.findElement(By.xpath("//button[@id='login-submit-btn']")).click();
-	}
-
-	@Then("^close the browser$")
-	public void tearDown() {
-
-		driver.close();
-
-	}
-
-}
+//package stepDefinitions;
+//
+//import java.util.Map;
+//import java.util.concurrent.TimeUnit;
+//
+//import org.junit.Assert;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//
+//import cucumber.api.DataTable;
+//import cucumber.api.java.en.Given;
+//import cucumber.api.java.en.Then;
+//import cucumber.api.java.en.When;
+//
+//public class FlightBookingMapStepDefinition {
+//	
+//	//data table with maps for parameterization
+//
+//	WebDriver driver;
+//
+//	@Given("^User is already on Welcome Page$")
+//	public void user_already_on_welcome_page() {
+//
+//		System.setProperty("webdriver.chrome.driver",
+//				"C:\\Users\\Suraj\\Desktop\\Selenium Jar\\Chrome Driver\\chromedriver.exe");
+//
+//		driver = new ChromeDriver();
+//
+//		driver.manage().window().maximize();
+//		driver.manage().deleteAllCookies();
+//		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+//		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+//
+//		driver.get("http://www.yatra.com");
+//
+//	}
+//
+//	@When("^title of login page is Yatra$")
+//	public void title_of_welcome_page_is_yatra() {
+//		String title = driver.getTitle();
+//		System.out.println(title);
+//		Assert.assertEquals("Flight, Cheap Air Tickets , Hotels, Holiday, Trains Package Booking - Yatra.com", title);
+//	}
+//
+//	@Then("^User clicks on login My Account dropdown$")
+//	public void user_clicks_on_login_My_Account_dropdown() {
+//		driver.findElement(By.xpath("//a[@class='dropdown-toggle' and text()='My Account']")).click();
+//	}
+//
+//	@Then("^login button is visible$")
+//	public void login_button_is_visible() {
+//		boolean loginButton = driver.findElement(By.id("signInBtn")).isDisplayed();
+//		Assert.assertEquals(true, loginButton);
+//	}
+//
+//	@Then("^User clicks login button$")
+//	public void loginButtonClickTest() {
+//		driver.findElement(By.id("signInBtn")).click();
+//	}
+//
+//	@Then("user enters username$")
+//	public void userNameFieldTest(DataTable username) {
+//
+//		for(Map<String, String> data1 : username.asMaps(String.class, String.class))
+//		driver.findElement(By.xpath("//input[@id='login-input']")).sendKeys(data1.get("username"));
+//	}
+//
+//	@Then("^user clicks on cont button$")
+//	public void continueButtonTest() {
+//		driver.findElement(By.xpath("//button[@id='login-continue-btn']")).click();
+//
+//	}
+//
+//	@Then("user enters password$")
+//	public void passwordFieldTest(DataTable password) {
+//		for (Map<String, String> data2 : password.asMaps(String.class, String.class)) {
+//			
+//			driver.findElement(By.xpath("//input[@id='login-password']")).sendKeys(data2.get("password"));
+//		
+//		}
+//	}
+//
+//	@Then("^user clicks on login button on login page$")
+//	public void loginButtonTest() {
+//		driver.findElement(By.xpath("//button[@id='login-submit-btn']")).click();
+//	}
+//
+//	@Then("^close the browser$")
+//	public void tearDown() {
+//
+//		driver.close();
+//
+//	}
+//
+//}
